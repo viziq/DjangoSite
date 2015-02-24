6ef8ceffeef397dc2b8ff65691936eb5c1cb5ad3
Vitaliy <6867145@ukr.net>
2015-02-24 20:30:07 +0200

    Update and rename loginsys to loginsys/urls.py

---
+++ loginsys/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = 'macpro'
+
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'firstapp.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^login/', 'loginsys.views.login'),
+    url(r'^logout/', 'loginsys.views.logout'),
+    url(r'^register/', 'loginsys.views.register'),
+)	
