af1258bb572a6fc2ce5f75ffa6555721b4088e6c
Vitaliy <6867145@ukr.net>
2015-02-24 20:08:37 +0200

    Update and rename article to article/models.py

---
+++ article/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from django.db 					import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Article(models.Model):
+	class Meta():
+		db_table = "article"
+	article_title = models.CharField(max_length = 200)
+	article_text = models.TextField()
+	article_date = models.DateTimeField()
+	article_likes = models.IntegerField(default=0)
+	
+class Comments(models.Model):
+	class Meta():	
+		db_table = 'comments'
+	comments_date = models.DateTimeField(auto_now_add = True)
+	comments_text = models.TextField(verbose_name="Текст комментария")
+	comments_article = models.ForeignKey(Article)
+	comments_from = models.ForeignKey(User)
